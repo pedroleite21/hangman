f9ef53d6557f72c0f432f43da0f526d7068d5390
Linaya <Angelochec6@gmail.com>
2016-05-21 14:57:22 +0600

    testing gtk interface creating

--- src/main.c
+++ src/main.c
@@ -5,8 +5,45 @@
 #include "word_select.h"
 #include "hangman.h"
 
+#define UI_FILE "../res/interface.glade"
+
+GtkBuilder *builder;
+GtkWidget *difficultWindow, *gameWindow, *msgWindow;
+GtkRadioButton *difficult0, *difficult1, *difficult2, *difficult3;
+GtkButton *startButton, *exitButton;
+
 int main(int argc, char *argv[])
 {
+    GError *error = NULL;
+
+    gtk_init( &argc, &argv );
+    builder = gtk_builder_new();
+    if( ! gtk_builder_add_from_file( builder, UI_FILE, &error ) )
+    {
+        g_warning( "%s", error->message );
+        g_free( error );
+        return( 1 );
+    }
+
+    difficultWindow = GTK_WIDGET(gtk_builder_get_object(builder, "difficultWindow"));
+    gameWindow = GTK_WIDGET(gtk_builder_get_object(builder, "gameWindow"));
+    msgWindow = GTK_WIDGET(gtk_builder_get_object(builder, "msgWindow"));
+    difficult0 = GTK_RADIO_BUTTON(gtk_builder_get_object(builder, "difficult0"));
+    difficult1 = GTK_RADIO_BUTTON(gtk_builder_get_object(builder, "difficult1"));
+    difficult2 = GTK_RADIO_BUTTON(gtk_builder_get_object(builder, "difficult2"));
+    difficult3 = GTK_RADIO_BUTTON(gtk_builder_get_object(builder, "difficult3"));
+    startButton = GTK_BUTTON(gtk_builder_get_object(builder, "startButton"));
+    exitButton = GTK_BUTTON(gtk_builder_get_object(builder, "exitButton"));
+
+    gtk_builder_connect_signals (builder, NULL);
+    g_object_unref( G_OBJECT( builder ) );
+    gtk_widget_show( difficultWindow );
+    gtk_widget_show( gameWindow );
+    gtk_widget_show( msgWindow );
+    gtk_main();
+
+    return 0;
+
     int gamesWin = 0, gamesLose = 0;
     char prompt = 'y';
     while (prompt != 'n')
@@ -48,4 +85,9 @@ int main(int argc, char *argv[])
     printf("\n");
     
     return 0;
+}
+
+void difficult_ch(GtkToggleButton *button)
+{
+    printf("FIX IT!\n");
 }
